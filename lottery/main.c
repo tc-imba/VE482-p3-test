d2918352e814597738bf2fe40f78ac38acc2b6c6
tc-imba <liuyh615@sjtu.edu.cn>
2017-12-15 00:50:27 +0800

    use clock

--- lottery/main.c
+++ lottery/main.c
@@ -6,21 +6,23 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
+#include <time.h>
 
 int ticket = 1;
 int exitflag = 0;
 pid_t pid = 0;
-unsigned long long counter = 0;
+unsigned long counter = 0;
+clock_t start;
 
 void sigint_cb(int signal)
 {
     exitflag = 1;
-    printf("pid:%d\tticket:%d\tcounter:%lu\n", getpid(), ticket, counter);
+    printf("pid:%d\tticket:%d\ttime:%lu\n", getpid(), ticket, clock() - start);
 }
 
 int main(int argc, char *argv[])
 {
-
+    start = clock();
     pid = fork();
     if (pid)
     {
