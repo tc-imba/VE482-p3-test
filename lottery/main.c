6d7993c90b21eb7e4942181f8b89290f8d9d5df6
tc-imba <liuyh615@sjtu.edu.cn>
2017-12-14 18:58:22 +0800

    change

--- lottery/main.c
+++ lottery/main.c
@@ -7,9 +7,6 @@
 #include <stdio.h>
 #include <signal.h>
 
-const int SIZE = 8;
-const int tickets[] = {0, 0, 0, 0, 2, 4, 6, 8};
-
 int ticket = 1;
 int exitflag = 0;
 pid_t pid = 0;
@@ -18,34 +15,23 @@ size_t counter = 0;
 void sigint_cb(int signal)
 {
     exitflag = 1;
-    if (!pid)
-    {
-        printf("pid:%d\tticket:%d\tcounter:%lu\n", getpid(), ticket, counter);
-    }
+    printf("pid:%d\tticket:%d\tcounter:%lu\n", getpid(), ticket, counter);
 }
 
 int main(int argc, char *argv[])
 {
 
-    for (int i = 0; i < SIZE; i++)
+    pid = fork();
+    if (!pid)
     {
-        pid = fork();
-        if (!pid)
-        {
-            sleep(10);
-            if (tickets[i] > 0)
-            {
-                nice(ticket = tickets[i]);
-            }
-            break;
-        }
+        nice(ticket = 10);
     }
 
     signal(SIGINT, sigint_cb);
 
     while (!exitflag)
     {
-        if (!pid) counter++;
+        counter++;
     }
 
 
